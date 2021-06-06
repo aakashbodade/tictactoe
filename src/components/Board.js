bcf79c9ae778782f750ca8687b4e0ca8711fef1e
Navin Bodade <aakashbodade1990@gmail.com>
2021-06-06 18:55:49 +0530

    added styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,18 +4,18 @@ import Square from './Square'
 
 const Board = () => {
     return (
-        <div>
-          <div>
+        <div className="board">
+          <div className="board-row">
              <Square Value={0}/>
              <Square Value={1}/> 
              <Square Value={2}/>  
          </div>
-          <div>
+          <div className="board-row">
              <Square Value={3}/>
              <Square Value={4}/> 
              <Square Value={5}/> 
          </div> 
-          <div>
+          <div className="board-row">
              <Square Value={6}/>
              <Square Value={7}/> 
              <Square Value={8}/>               
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+
+
 const Square = ({Value}) => {
    
-   return <button type="button">{Value}</button>
+   return <button type="button" className="square" >{Value}</button>
     
 };
 
